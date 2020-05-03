400ce9fb01581558a67b8d595de31c031795d3b3
wizardoma <alibekason@gmail.com>
2020-05-03 20:37:48 +0100

    Initialized project

---
+++ src/main/java/com/techbek/coronavirus/CoronavirusApplication.java
@@ -0,0 +1,13 @@
+package com.techbek.coronavirus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronavirusApplication.class, args);
+    }
+
+}
