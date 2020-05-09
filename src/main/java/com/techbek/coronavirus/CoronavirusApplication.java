e9a74e008484aef6fb94e6515d97a6bbe1f9b13e
wizardoma <alibekason@gmail.com>
2020-05-09 11:48:20 +0100

    added client to load raw coronavirus

--- src/main/java/com/techbek/coronavirus/CoronavirusApplication.java
+++ src/main/java/com/techbek/coronavirus/CoronavirusApplication.java
@@ -2,8 +2,11 @@ package com.techbek.coronavirus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronavirusApplication {
 
     public static void main(String[] args) {
